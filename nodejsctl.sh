8cf4ec24e0924c50ba6546b0ed228c0b3aca1af6
Jackson Tian <shyvo1987@gmail.com>
2015-11-23 12:07:25 +0800

    refine nodejsctl.sh

--- nodejsctl.sh
+++ nodejsctl.sh
@@ -6,11 +6,14 @@ export NODE_ENV='production'
 #set ulimit
 ulimit -c unlimited
 
-NODEJS='node --harmony'
+#set for alinode
+export ENABLE_NODE_LOG=YES
+export NODE_LOG_DIR=/tmp/
+
+NODEJS='node'
 BASE_HOME=`pwd`
 PROJECT_NAME=`basename ${BASE_HOME}`
-STDOUT_LOG=`$NODEJS -e "console.log(require('path').join(require('$BASE_HOME/config').logdir, 'nodejs_stdout.log'));\
-process.exit(0);"`
+STDOUT_LOG=`$NODEJS -p "require('path').join(require('$BASE_HOME/config').logdir, 'nodejs_stdout.log')"`
 NODE_DISPATH_PATH=${BASE_HOME}/dispatch.js
 PROG_NAME=$0
 ACTION=$1
@@ -30,17 +33,17 @@ function get_pid {
 #start nodejs
 start()
 {
-   get_pid
-   if [ -z $PID ]; then
-      echo "Starting $PROJECT_NAME ..."
-      echo "nohup $NODEJS $NODE_DISPATH_PATH > $STDOUT_LOG 2>&1 &"
-      nohup $NODEJS $NODE_DISPATH_PATH > $STDOUT_LOG  2>&1 &
-      sleep 2
-      get_pid
-      echo "Start nodejs success. PID=$PID"
-   else
-      echo "$PROJECT_NAME is already running, PID=$PID"
-   fi
+  get_pid
+  if [ -z $PID ]; then
+    echo "Starting $PROJECT_NAME ..."
+    echo "nohup $NODEJS $NODE_DISPATH_PATH > $STDOUT_LOG 2>&1 &"
+    nohup $NODEJS $NODE_DISPATH_PATH > $STDOUT_LOG  2>&1 &
+    sleep 2
+    get_pid
+    echo "Start nodejs success. PID=$PID"
+  else
+    echo "$PROJECT_NAME is already running, PID=$PID"
+  fi
 }
 
 stop()
@@ -57,7 +60,7 @@ stop()
       ipcs -s | grep 0x | awk '{print $2}' | xargs -n1 ipcrm -s  > /dev/null 2>&1
       ipcs -m | grep 0x | awk '{print $2}' | xargs -n1 ipcrm -m  > /dev/null 2>&1
     fi
-  mv -f $STDOUT_LOG "${STDOUT_LOG}.`date '+%Y%m%d%H%M%S'`"
+    mv -f $STDOUT_LOG "${STDOUT_LOG}.`date '+%Y%m%d%H%M%S'`"
   else
     echo "$PROJECT_NAME is not running"
   fi
